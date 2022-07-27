df015c8ec867a2602e8924f50c61a1c5a10c9cab
MartinBulla <bulla.mar@gmail.com>
2022-07-27 17:31:49 +0200

    Update EXP_relatednessMat.R

--- R/EXP_relatednessMat.R
+++ R/EXP_relatednessMat.R
@@ -216,9 +216,9 @@
     save(wcv, file = paste0('Outputs/resRel_CV_',sample_,'.Rdata'))
     
 # Export summary
- #load('Outputs/temp_resRel_morpho.Rdata')
- #load('Outputs/temp_resRel_motil.Rdata')
- #load('Outputs/temp_resRel_CV.Rdata')
+ load('Outputs/resRel_morpho_5000.Rdata')
+ load('Outputs/resRel_motil_5000.Rdata')
+ load('Outputs/resRel_CV_5000.Rdata')
 
   wv[, data := 'June'] 
   wv[, type := 'motility'] 
@@ -237,24 +237,27 @@
   x[, prob:=round(prob,3)]
   x[, bf:=round(bf)]
   cols_=c('est', 'lwr','upr')
-  x[ , (cols_) := lapply(.SD, function(x){round(x*100,1)}), .SDcols = cols_]
-  x[substring(upr,1,1) != 0, upr := round(upr)]
-  x[substring(lwr,1,1) != 0, lwr := round(lwr)]
+  #x[ , (cols_) := lapply(.SD, function(x){round(x*100,1)}), .SDcols = cols_]
+  #x[substring(upr,1,1) != 0, upr := round(upr)]
+  #x[substring(lwr,1,1) != 0, lwr := round(lwr)]
+  #x[, est := paste0(est, '%')]
+  #x[, CI := paste0(lwr, '-', upr, '%')]
+  
+  x[ , (cols_) := lapply(.SD, function(x){format(round(x*100,1), nsmall = 1)}), .SDcols = cols_]
   x[, est := paste0(est, '%')]
-  x[, CI := paste0(lwr, '-', upr, '%')]
+  x[, lwr := paste0(lwr,'%')]
+  x[, upr := paste0(upr,'%')]
 
-
-  summary(x[data=='all',.(est, lwr, upr, bf, prob, Rhat_no, Rhat_yes)])
-  summary(x[data!='all',.(est, lwr, upr, bf, prob, Rhat_no, Rhat_yes)])
-
-  y = x[,.(type, data, response, est, CI, bf, prob, Rhat_no, Rhat_yes)]
+  y = x[,.(data, type, response, est, lwr, upr, bf, prob, Rhat_no, Rhat_yes)]
   y[, data := factor(data, levels=c("all","June","means", 'CV'))] 
   y[, type := factor(type, levels=c("motility","morphology"))] 
-  y[, response := factor(response, levels=c("Curvilinear","Average path",'Straight line',"Acrosome", "Nucleus",  "Midpiece","Tail","Total", "Head","Flagellum"))] 
+  y[, response := factor(response, levels=c("Curvilinear",'Straight line',"Average path","Acrosome", "Nucleus",  "Midpiece","Tail","Total", "Head","Flagellum"))] 
 
+  #summary(x[data=='all',.(est, lwr, upr, bf, prob, Rhat_no, Rhat_yes)])
+  #summary(x[data!='all',.(est, lwr, upr, bf, prob, Rhat_no, Rhat_yes)])
 
   fwrite(y[order(data, type,response)], file = paste0('Outputs/Table_Srelate_',sample_,'.csv'))
-
+ 
 # explore chains
   ggplot(b, aes(x = Length_µm))+facet_wrap(~part, scales = 'free') + geom_density()
   ggplot(b, aes(x = Length_µm))+facet_wrap(~part, scales = 'free') + geom_histogram()
