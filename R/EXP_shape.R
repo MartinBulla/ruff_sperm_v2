2ea8e7c2544a0eb761da6ed088e20a9ab2cddd41
MartinBulla <bulla.mar@gmail.com>
2022-10-20 17:13:49 +0200

    Create EXP_shape.R

---
+++ R/EXP_shape.R
@@ -0,0 +1,20 @@
+# TOOLS
+  require(here)
+  source(here::here('R/tools.R'))
+
+# DATA
+  source(here::here('R/DAT_prepare.R'))       
+
+# relative values
+  bw[,h_rel:=Head/Total]
+  bw[,n_rel_h:=Nucleus/Head]
+  bw[,t_rel:=Tail/Total]
+  bw[,h_rel_tail:=Head/Tail]
+  bw[,h_rel_flag:=Head/Flagellum]
+  bw[,t_rel_flag:=Tail/Flagellum]
+
+  summary(bw)
+  ggplot(bw, aes(x=h_rel_tail, y = h_rel_flag)) + geom_point()
+  ggplot(bw, aes(x=h_rel_tail, y = Flagellum_rel)) + geom_point()
+ 
+ # END
