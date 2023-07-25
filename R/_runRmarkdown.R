1eb54c4aae7e4cc5d8dce394b3d183d45d99722c
MartinBulla <bulla.mar@gmail.com>
2023-07-25 17:52:38 +0200

    updated velocity video generation

--- R/_runRmarkdown.R
+++ R/_runRmarkdown.R
@@ -3,7 +3,7 @@ require(rmarkdown)
 rmarkdown::render('R/Out_velocity_videos_examples.R', output_dir = 'Outputs',  output_file = 'Velocity_video_examples_test.html')
 rmarkdown::render('R/Out_velocity-videos.Rmd', output_dir = 'Outputs',  output_file = 'Velocity_video_examples.html')
 
-rmarkdown::render('R/Preregistration_v3.Rmd', output_dir = 'Protocols')
+rmarkdown::render('Protocols/Preregistration_v3.Rmd', output_dir = 'Protocols')
 
-rmarkdown::render('R/protocol_sperm.Rmd', output_dir = 'Protocols')
+rmarkdown::render('Protocols/protocol_sperm.Rmd', output_dir = 'Protocols')
 #rmarkdown::render('R/protocol_sperm.Rmd', output_dir = 'Protocols', word_document(toc = TRUE))
