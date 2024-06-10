5f9ef5dac22e37084832f7787775d2ba5baa2118
MartinBulla <bulla.mar@gmail.com>
2024-06-10 12:11:18 +0200

    Update _runRmarkdown.R

--- R/_runRmarkdown.R
+++ R/_runRmarkdown.R
@@ -6,6 +6,7 @@
 
 require(rmarkdown)
 
+rmarkdown::render("R/Out_velocity-videos_anonym.Rmd", output_dir = "Outputs", output_file = "Velocity_video_examples_anonym.html") # rmarkdown::render('R/Out_velocity_videos_examples.R', output_dir = 'Outputs',  output_file = 'Velocity_video_examples_test.html')
 rmarkdown::render("R/Out_velocity-videos.Rmd", output_dir = "Outputs", output_file = "Velocity_video_examples.html") # rmarkdown::render('R/Out_velocity_videos_examples.R', output_dir = 'Outputs',  output_file = 'Velocity_video_examples_test.html')
 
 rmarkdown::render('Protocols/Preregistration_v3.Rmd', output_dir = 'Protocols')
